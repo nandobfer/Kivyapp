0e3c77cb34210657b79d851489d5a04ec625fefd
Burgos <nandobfer@yahoo.com.br>
2022-06-10 12:02:36 -0300

    first kivy

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+# from kivy.uix.label import Label
+# from kivy.uix.button import Button
+# from kivy.uix.boxlayout import BoxLayout
+
+
+class FirstApp(App):
+    pass
+
+
+FirstApp().run()
